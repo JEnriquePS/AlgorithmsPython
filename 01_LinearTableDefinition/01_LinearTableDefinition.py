8957df43936cf4aa7fe16a6c4e7bf9803488fd4e
elYiiiisus <jenrique.ps@gmail.com>
2024-09-30 17:57:29 -0500

    LinearTableDefinition

---
+++ 01_LinearTableDefinition/01_LinearTableDefinition.py
@@ -0,0 +1,12 @@
+"""Linear Table:
+Sequence of elements, is a one-dimensional array.
+"""
+
+# Create a list of numbers
+def main():
+    scores = [90, 85, 75, 60, 50, 45, 40, 30, 20, 10]
+    # Print the list
+    print(','.join(map(str, scores)))
+
+if __name__ == "__main__":
+    main()
